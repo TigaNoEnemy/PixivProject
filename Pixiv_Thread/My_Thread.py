1c49236828f5da39e5f2e6a6dc558a1ea1577448
minming <minming>
2020-12-02 17:44:52 +0800

    使用concurrent模块代替QThreadPool，真正地实现了线程池管理（QThreadPool中的线程无法发送信号，垃圾）

--- Pixiv_Thread/My_Thread.py
+++ Pixiv_Thread/My_Thread.py
@@ -1,8 +1,8 @@
-
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from PyQt5.QtCore import QThread, pyqtSignal, QMutex
+from PyQt5.QtCore import pyqtSignal, QObject
+from concurrent.futures import ThreadPoolExecutor
 import pixivpy3
 import time
 
@@ -10,32 +10,13 @@ import time
 import cgitb
 cgitb.enable(format='text', logdir='log_file')
 
-class QMutex_Manager:
-    thread_pool = 10     # 线程限制在thread_pool个
-    wait_mutex = 0      # 当没有闲置锁时，在第wait_mutex个锁等待
-    mutex_status = {i: True for i in range(thread_pool)}
-    mutex_box = {i: QMutex() for i in range(thread_pool)}
 
-    @classmethod
-    def get_mutex(cls, *args, **kwargs):
-        for i in cls.mutex_status:
-            if cls.mutex_status[i]:
-                return i
+THREAD_POOL = ThreadPoolExecutor(max_workers=5)
 
-        # 没有闲置锁，在第i个等待
-        i = cls.wait_mutex
-        cls.wait_mutex += 1
-        if cls.wait_mutex >= cls.thread_pool:
-            cls.wait_mutex = 0
-        return i
-
-
-class base_thread(QThread):
+class base_thread(QObject):
     # 除更新UI以外的其他操作
     """docstring for DownloadThread"""
     finish = pyqtSignal(dict)
-    none_finish = pyqtSignal()
-    thread_pool = 5     # 特定线程限制在thread_pool个
     root = None
 
     def __init__(self, parent, method, info={}, **args):
@@ -44,27 +25,16 @@ class base_thread(QThread):
         self.args = args
         self.info = info
 
-    #@profile
-    def run(self):
-        lock_id = QMutex_Manager.get_mutex()
-        QMutex_Manager.mutex_box[lock_id].lock()
-        QMutex_Manager.mutex_status[lock_id] = False
-        try:
-            a = self.method(**self.args)
-        except pixivpy3.utils.PixivError:
-            error_info = {'info': self.info}    # 传递给连接好的函数
-            error_info.update({'ERROR': True})  
-            error_info.update({'method': self.method, 'args': self.args})   # 提供网络错误时执行的函数及该函数所需的参数
-            self.finish.emit(error_info)
-        else:
-            if isinstance(a, dict):
-                a.update(self.info)
-                self.finish.emit(a)
-            else:
-                self.none_finish.emit()
+    def start(self):
+        future = THREAD_POOL.submit(self.method, **self.args)
+        future.add_done_callback(self.emit_signal)
 
-        QMutex_Manager.mutex_box[lock_id].unlock()
-        QMutex_Manager.mutex_status[lock_id] = True
+    def emit_signal(self, future):
+        result = future.result()
+        self.finish.emit(result)
+
+    def wait(self):
+        pass
 
 
 if __name__ == '__main__':
